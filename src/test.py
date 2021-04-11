9c619bd12aec3a41192253458cf9ab805cb2dc90
Dominic Littlewood <11dlittlewood@gmail.com>
2021-04-11 11:06:11 +0100

    Fix translations of text with "on" and "if"

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ from translate import DUNGEONS_ROOT
 shutil.rmtree(DUNGEONS_ROOT + 'mods/emoji', ignore_errors=True)
 shutil.copytree('../emoji', DUNGEONS_ROOT + 'mods/emoji')
 process = subprocess.Popen([DUNGEONS_ROOT + 'diceydungeons.exe',
-                            'mod=emoji'],
+                            'mod=emoji', '-translator'],
                            stdout=subprocess.PIPE)
 
 for line in process.stdout:
